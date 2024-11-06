a9a32b40a3e1e7782a1db0d20d97b7878fd714a1
muhsi <pmuhsin20@gmail.com>
2024-11-06 11:58:57 +0400

    06 / 11/24

---
+++ 258.py
@@ -0,0 +1,12 @@
+#1-Create a class named Car with a property named model.
+# Then, create an object named my_car and print the value of model
+
+class car:
+    model ="benz"
+mycar =(car)
+print(mycar.model)
+
+
+
+
+
---
+++ 259.py
@@ -0,0 +1,9 @@
+# 2-Create a class named Person with a property named name.
+# Instantiate an object called person1 and print the value of name?
+
+
+class person:
+    name = "ilyas"
+person1 =(person)
+
+print(person1.name)
